7a9192aa6b3e4da49513110605ff7c25696c021b
pkc1088 <84122393+pkc1088@users.noreply.github.com>
2022-03-16 12:36:59 +0900

    Add files via upload

---
+++ vscTest.py
@@ -0,0 +1,20 @@
+print("hello world")
+print('hello world')
+a = 100
+print(a)
+
+if a==100:
+    print("a is 100")
+else:
+    print("a is not 100")
+
+arr = [1, 2, 3, 4.4]
+print(arr)
+
+
+
+
+
+
+
+
